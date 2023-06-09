894c24cfae1e8c32dcd4587eda05db8f602f7fc2
Leonardo <cardosoleo588@gmail.com>
2023-06-09 16:13:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function sim(){
+    alert (" voce aceitou namorar comigo :)")
+   
+   }
+   function desvia (t){
+   var btn = t;
+   btn.style.position = 'absolute';
+   btn.style.bottom = geraPosicao (10, 90);
+   btn.style.left = geraPosicao (10, 90);
+   console.log ("opa, desviei....");
+   }
+     
+   function geraPosicao (min, max){
+       return (Math.random() * (max - min) + min) +"%";
+   
+   }
