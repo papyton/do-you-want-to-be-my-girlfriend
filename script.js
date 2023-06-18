381987f3ba63a7609fd626a17c5f564645967da9
Leonardo <cardosoleo588@gmail.com>
2023-06-18 12:19:50 -0300

    mudei o codigo

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function sim(){
    function desvia (t){
    var btn = t;
    btn.style.position = 'absolute';
-   btn.style.bottom = geraPosicao (10, 90);
+   btn.style.button = geraPosicao (10, 90);
    btn.style.left = geraPosicao (10, 90);
    console.log ("opa, desviei....");
    }
